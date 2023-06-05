fc2106379809765ea2dd72cf0f8d4863d8286d86
Raphael <demmerleraphael@hotmail.com>
2023-06-05 23:20:13 +0200

    changer close button + style on sidebar

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,11 @@
 import { useMainUrlStore } from '../../Store/store'
-import { Button, Card, Title } from '@tremor/react'
-import { useQueryGetAllCollectionsName, useQueryGetAllData } from '../../service/getAllData';
+import { Button, Title } from '@tremor/react'
+import { useQueryGetAllCollectionsName } from '../../service/getAllData';
 import GenericTable from '../GenericTable/GenericTable';
 import SideBar from '../SideBar/SideBar';
 import Loader from '../Loader/Loader';
 import { Suspense } from 'react';
+import LogoutIcon from '@heroicons/react/outline/LogoutIcon';
 
 const Home = () => {
     const [mainUrl, setMainUrl, collectionName] = useMainUrlStore(state => [state.mainUrl, state.setMainUrl, state.collectionName])
@@ -24,16 +25,21 @@ const Home = () => {
             <Suspense fallback={'loading...'}>
                 <div className='flex w-full'>
                     <SideBar collectionsName={data} />
-                    {collectionName && <Card className='w-full bg-slate-900'>
-                        <Title>{collectionName}</Title>
-                        <GenericTable collectionName={collectionName} />
-                        <div className='flex justify-center'>
-                            <Button onClick={() => {
-                                useMainUrlStore.persist.clearStorage()
-                                setMainUrl("")
-                            }} >Reset main url</Button>
+                    {collectionName &&
+                        <div className='w-full bg-slate-900 p-6'>
+                            <div className='flex items-center justify-center relative'>
+                                <Title className='text-4xl font-bold'>{collectionName}</Title>
+                                <div className='absolute right-0'>
+                                    <Button icon={LogoutIcon} color='red' onClick={() => {
+                                        useMainUrlStore.persist.clearStorage()
+                                        setMainUrl("")
+                                    }} >Quitter</Button>
+                                </div>
+
+                            </div>
+                            <GenericTable collectionName={collectionName} />
                         </div>
-                    </Card>}
+                    }
                 </div>
             </Suspense>
         </>
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -12,7 +12,7 @@ const SideBar = ({ collectionsName }: Props) => {
         <div className="h-screen bg-slate-800 p-5">
             <h2>EasyDashBoard</h2>
             <List>
-                {collectionsName.map(collectionsName => <ListItem onClick={() => setCollectionName(collectionsName.name)} className="hover:cursor-pointer" key={collectionsName.info.uuid}>{collectionsName.name}</ListItem>)}
+                {collectionsName.map(collectionsName => <ListItem onClick={() => setCollectionName(collectionsName.name)} className="hover:cursor-pointer text-2xl hover:text-white" key={collectionsName.info.uuid}>{collectionsName.name}</ListItem>)}
             </List>
         </div>
     )
